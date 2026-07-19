f1850a2c0dd0cca80db8f5c5470baefb2c3c9819
DivyanshuKashyap14 <divyanshukashyap140904@gmail.com>
2026-07-20 00:36:44 +0530

    Basic codes in cpp

--- lecture1.cpp
+++ lecture1.cpp
@@ -5,3 +5,31 @@
 //     cout<<"Hello Duniya";
 //     return 0;
 // }
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int a='a';
+//     cout<<a<<endl;
+//     char b=97;
+//     cout<<b<<endl;
+//     return 0;
+// }     //Typecasting
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter the number:"<<endl;
+    cin>>a;
+    cin>>b;
+    cout<<a+b<<endl;
+    cout<<a-b<<endl;
+    cout<<a*b<<endl;
+    cout<<a/b<<endl;
+    cout<<a%b<<endl;
+    return 0;
+}
