fbfc209692eedf73e8c4a48402b390e5def82146
DivyanshuKashyap14 <divyanshukashyap140904@gmail.com>
2026-07-26 13:13:48 +0530

    for loop practice

--- forloop.cpp
+++ forloop.cpp
@@ -48,36 +48,36 @@
 
 
 
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// using namespace std;
 
-int main()
-{
-    int n;
-    cin >> n;
+// int main()
+// {
+//     int n;
+//     cin >> n;
 
-    bool isPrime = true;
+//     bool isPrime = true;
 
-    if(n <= 1)
-    {
-        isPrime = false;
-    }
-    else
-    {
-        for(int i = 2; i < n; i++)
-        {
-            if(n % i == 0)
-            {
-                isPrime = false;
-                break;
-            }
-        }
-    }
+//     if(n <= 1)
+//     {
+//         isPrime = false;
+//     }
+//     else
+//     {
+//         for(int i = 2; i < n; i++)
+//         {
+//             if(n % i == 0)
+//             {
+//                 isPrime = false;
+//                 break;
+//             }
+//         }
+//     }
 
-    if(isPrime)
-        cout << "Prime Number";
-    else
-        cout << "Not a Prime Number";
+//     if(isPrime)
+//         cout << "Prime Number";
+//     else
+//         cout << "Not a Prime Number";
 
-    return 0;
-}
+//     return 0;
+// }
