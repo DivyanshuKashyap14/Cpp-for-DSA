ad2139869b617ad15a525a7d98ea1e067b657275
DivyanshuKashyap14 <divyanshukashyap140904@gmail.com>
2026-07-29 21:39:26 +0530

    Functions

--- function.cpp
+++ function.cpp
@@ -21,3 +21,23 @@
 // }
 
 
+#include<iostream>
+using namespace std;
+int factorial(int n){
+    int fact=1;
+    for (int i=1;i<=n;i++){
+        fact=fact*i;
+    }
+    return fact;
+}
+int nCr(int n, int r){
+    int num=fact(n);
+    int denom=fact(n-r)*fact(r);
+    int ans=num/denom;
+    return ans;
+}
+int main(){
+    int n,r; 
+}
+
+
