227efd86f2aa24c4f45aa28a4a773233b2a81cb8
DivyanshuKashyap14 <divyanshukashyap140904@gmail.com>
2026-07-31 15:04:52 +0530

    leetcode problems

--- leetcode.cpp
+++ leetcode.cpp
@@ -72,3 +72,19 @@
 //         return ans;
 //     }
 // };
+
+
+//231. power of 2
+
+// class Solution {
+// public:
+//     bool isPowerOfTwo(int n) {
+//         for(int i=0;i<=30;i++){
+//             int ans=pow(2,i);
+//             if(ans==n){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// };
