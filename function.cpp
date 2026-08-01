fb81c247c65e3f5f110720065a96c2ede4a5cb44
DivyanshuKashyap14 <divyanshukashyap140904@gmail.com>
2026-08-01 13:25:35 +0530

    Functions

--- function.cpp
+++ function.cpp
@@ -21,23 +21,42 @@
 // }
 
 
-#include<iostream>
-using namespace std;
-int factorial(int n){
-    int fact=1;
-    for (int i=1;i<=n;i++){
-        fact=fact*i;
-    }
-    return fact;
-}
-int nCr(int n, int r){
-    int num=fact(n);
-    int denom=fact(n-r)*fact(r);
-    int ans=num/denom;
-    return ans;
-}
-int main(){
-    int n,r; 
-}
+// #include<iostream>
+// using namespace std;
+// int factorial(int n){
+//     int fact=1;
+//     for (int i=1;i<=n;i++){
+//         fact=fact*i;
+//     }
+//     return fact;
+// }
+// int nCr(int n, int r){
+//     int num=fact(n);
+//     int denom=fact(n-r)*fact(r);
+//     int ans=num/denom;
+//     return ans;
+// }
+// int main(){
+//     int n,r; 
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int power(int a,int b){
+//     int ans=1;
+//     for (int i=1;i<=b;i++){
+//         ans=ans*(a);
+//     }
+//     return ans;
+// }
+// int main()
+// {
+//     int a,b;
+//     cin>>a>>b;
+//     int answer=power(a,b);
+//     cout<<"answer is "<<answer<<endl;
+//     return 0;
+// }
 
 
