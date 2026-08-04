12cdf286b97c743b095ef88a4e6f5cfa666e1c32
DivyanshuKashyap14 <divyanshukashyap140904@gmail.com>
2026-08-04 13:19:06 +0530

    arrays practice

--- arrays.cpp
+++ arrays.cpp
@@ -1,38 +1,38 @@
-#include <iostream>
-#include <climits>   // for INT_MAX and INT_MIN
-using namespace std;
+// #include <iostream>
+// #include <climits>   // for INT_MAX and INT_MIN
+// using namespace std;
 
-int getMin(int num[], int n) {
-    int min = INT_MAX;
-    for (int i = 0; i < n; i++) {
-        if (min > num[i]) {
-            min = num[i];
-        }
-    }
-    return min;
-}
+// int getMin(int num[], int n) {
+//     int min = INT_MAX;
+//     for (int i = 0; i < n; i++) {
+//         if (min > num[i]) {
+//             min = num[i];
+//         }
+//     }
+//     return min;
+// }
 
-int getMax(int num[], int n) {
-    int max = INT_MIN;
-    for (int i = 0; i < n; i++) {
-        if (max < num[i]) {
-            max = num[i];
-        }
-    }
-    return max;
-}
+// int getMax(int num[], int n) {
+//     int max = INT_MIN;
+//     for (int i = 0; i < n; i++) {
+//         if (max < num[i]) {
+//             max = num[i];
+//         }
+//     }
+//     return max;
+// }
 
-int main() {
-    int size;
-    cin >> size;
+// int main() {
+//     int size;
+//     cin >> size;
 
-    int num[100];
-    for (int i = 0; i < size; i++) {
-        cin >> num[i];
-    }
+//     int num[100];
+//     for (int i = 0; i < size; i++) {
+//         cin >> num[i];
+//     }
 
-    cout << "Max element: " << getMax(num, size) << endl;
-    cout << "Min element: " << getMin(num, size) << endl;
+//     cout << "Max element: " << getMax(num, size) << endl;
+//     cout << "Min element: " << getMin(num, size) << endl;
 
-    return 0;
-}
+//     return 0;
+// }
