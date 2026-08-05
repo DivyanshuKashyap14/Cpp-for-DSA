9a3fc210ed90dd9ea0aa2885e825ca7983895f6b
DivyanshuKashyap14 <divyanshukashyap140904@gmail.com>
2026-08-05 12:51:06 +0530

    arrays practice

--- arrays.cpp
+++ arrays.cpp
@@ -36,3 +36,82 @@
 
 //     return 0;
 // }
+
+
+
+// #include<iostream>
+// using namespace std;
+// bool search(int arr[],int size, int key){
+//     for(int i=0;i<size;i++){
+//         if(arr[i]==key){
+//             return 1;
+//         }
+
+//     }
+//     return 0;
+
+// }
+// int main()
+// {
+//     int arr[10]={2, 5, 6, 1, 2, 3, 4, 5, 9, 10 };
+//     int key;
+//     cin>>key;
+//     bool found = search(arr,10,key);
+//     if(found){
+//         cout<<"key is present"<<endl;
+//     }
+//     else{
+//         cout<<"key is absent"<<endl;
+//     }
+//     return 0;
+// }                                 //LINEAR SEARCH
+
+
+// #include<iostream>
+// using namespace std;
+// void reverse(int arr[], int n) {
+//     for(int i = 0; i < n - 1; i += 2) {
+//         swap(arr[i], arr[i + 1]);
+//     }
+// }
+// void printarray(int arr[],int n){
+//     for(int i=0;i<n;i++){
+//         cout<<arr[i]<<" ";
+//     }
+//     cout<<endl;
+// }
+// int main()
+// {
+//     int arr[6]={1,2,3,4,5,6};
+//     int brr[6]={7,8,9,10,11,5};
+//     reverse(arr,6);
+//     reverse(brr,6);
+//     printarray(arr,6);
+//     printarray(brr,6);
+//     return 0;
+// }                                  REVERSING THE ARRAY ALTERNATELY
+
+
+// #include<iostream>
+// using namespace std;
+
+// int sumarray(int arr[], int n)
+// {
+//     int sum = 0;
+
+//     for(int i = 0; i < n; i++)
+//     {
+//         sum += arr[i];
+//     }
+
+//     return sum;
+// }
+
+// int main()
+// {
+//     int arr[5] = {1, 2, 3, 4, 5};
+
+//     cout << "Sum = " << sumarray(arr, 5);
+
+//     return 0;                               SUM OF AN ARRAY
+// }
