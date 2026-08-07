821055677bfb018a25d8a378fbfee1cd38f20eb3
DivyanshuKashyap14 <divyanshukashyap140904@gmail.com>
2026-08-07 14:17:40 +0530

    LEETCODE PROBLEMS

--- leetcode.cpp
+++ leetcode.cpp
@@ -111,3 +111,18 @@
 //         return answer;
 //     }
 // };
+
+
+// 189. REVERSE THE ARRAY TO THE Kth ELEMENTS
+
+// class Solution {
+// public:
+//     void rotate(vector<int>& nums, int k) {
+//         int n=nums.size();
+//         k=k%n;
+//         reverse(nums.begin(),nums.end()-k);
+//         reverse(nums.end()-k,nums.end());
+//         reverse(nums.begin(),nums.end());
+        
+//     }
+// };
