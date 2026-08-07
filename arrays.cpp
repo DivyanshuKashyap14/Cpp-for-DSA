bdcf00d492f909c9ce8d16ad4f9c012ed8c5b721
DivyanshuKashyap14 <divyanshukashyap140904@gmail.com>
2026-08-07 21:52:51 +0530

    LEETCODE PROBLEMS

--- arrays.cpp
+++ arrays.cpp
@@ -168,3 +168,25 @@
 //     cout<<"Sum= "<<getsum(arr,n);                        SUM OF THE ELEMENTS IN THE ARRAY
 //     return 0;
 // }
+
+
+//485. COUNT THE MAX NUMBER OF ONES
+
+// class Solution {
+// public:
+//     int findMaxConsecutiveOnes(vector<int>& nums) {
+//         int count=0;
+//         int maxx=0;
+//         for(int i=0;i<nums.size();i++){
+//             if(nums[i]==1){
+//                 count++;
+//                 maxx=max(maxx,count);
+//             }
+//             else{
+//                 count=0;
+//             }
+//         }
+//         return maxx;
+        
+//     }
+// };
