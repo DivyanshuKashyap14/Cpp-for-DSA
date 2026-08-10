66bf87977c9714b0736bdf048e5ecea733717b52
DivyanshuKashyap14 <divyanshukashyap140904@gmail.com>
2026-08-10 14:58:47 +0530

    STL

---
+++ STL.cpp
@@ -0,0 +1,42 @@
+// #include<iostream>
+// #include<deque>
+// using namespace std;
+// int main()
+// {
+//     deque<int>d;
+// d.push_back(1);
+// d.push_front(2);
+// cout<<endl;
+// cout<<"Print first index elemant: "<<d.at(1)<<endl;
+// cout<<"front "<<d.front()<<endl;
+// cout<<"back "<<d.back()<<endl;
+// cout<<"empty or not  "<<d.empty()<<endl;
+// cout<<"before erase:"<<d.size()<<endl;
+// d.erase(d.begin(),d.begin()+1);
+// cout<<"afer erase: "<<d.size()<<endl;
+// for(int i:d){
+//     cout<<i<<endl;
+// }
+// }                                    DEQUE STL
+
+
+// #include<iostream>
+// #include<list>
+// using namespace std;
+// int main()
+// {
+//     list<int>l;
+//     l.push_back(1);
+//     l.push_front(2);
+//     for(int i:l){
+//         cout<<i<<" ";
+//     }
+//     cout<<endl;
+//     l.erase(l.begin());
+//     cout<<"after erase: "<<endl;
+//     for(int i:l){
+//         cout<<i<<" ";
+//     }
+// }                           LIST IN STL
+
+
