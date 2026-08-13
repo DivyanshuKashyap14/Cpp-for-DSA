53e622e362b1fd65fdaaa0adc4979dbb9f2c51f3
DivyanshuKashyap14 <divyanshukashyap140904@gmail.com>
2026-08-13 14:38:23 +0530

    arrays practice

--- arrays.cpp
+++ arrays.cpp
@@ -190,3 +190,29 @@
         
 //     }
 // };
+
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int arr[5]={1,2,3,4,5};
+//     reverse(arr,arr+5);
+//     for(int i=0;i<5;i++){
+//         cout<<arr[i]<<" ";
+//     }
+// }
+
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int arr[9]={1,2,3,4,5,6,7,8,9};
+//     for(int i=0;i<9/2;i++){
+//         swap(arr[i],arr[9-i-1]);
+//     }
+//     for(int i=0;i<9;i++){
+//         cout<<arr[i]<<" ";
+//     }
+// }
