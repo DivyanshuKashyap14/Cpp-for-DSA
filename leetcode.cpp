bacd59e669e7c39bec8d34d0ccc6efc0da377ce2
DivyanshuKashyap14 <divyanshukashyap140904@gmail.com>
2026-08-20 15:35:29 +0530

    LEETCODE PROBLEMS

--- leetcode.cpp
+++ leetcode.cpp
@@ -172,3 +172,21 @@
         
 //     }
 // };
+
+
+// 69. SQRT ROOT (NEWTON'S METHOD)
+
+// class Solution {
+//   public:
+//     int mySqrt(int x) {
+//         if(x==0){
+//             return 0;
+//         }
+//         long long r=x;
+//         while(r*r>x){
+//              r=(r+x/r)/2;
+//         }
+//         return r;
+        
+//     }
+// };
